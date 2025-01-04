0668e61b8dda2c19f745e46bcd9d1f250d44a39c
Marcos Vidal Martinez <marcos.vidal.m@outlook.com>
2025-01-04 07:03:49 +0100

    Update lock.py
    added basic imports

--- custom_components/securitas/lock.py
+++ custom_components/securitas/lock.py
@@ -1,30 +1,20 @@
+"""Securitas direct sentinel sensor."""
+
+import logging
+from typing import TYPE_CHECKING, Any
+
 from homeassistant.components.lock import LockEntity
-from .securitas_api import SecuritasDirectAPI
+from homeassistant.const import ATTR_CODE
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-class SecuritasDirectLock(LockEntity):
-    def __init__(self, api: SecuritasDirectAPI, device_id: str, name: str):
-        self._api = api
-        self._device_id = device_id
-        self._name = name
-        self._is_locked = None
+from . import CONF_INSTALLATION_KEY, DOMAIN, SecuritasDirectDevice, SecuritasHub
+from .securitas_direct_new_api.dataTypes import Lock, Service
 
-    @property
-    def name(self):
-        return self._name
+_LOGGER = logging.getLogger(__name__)
 
-    @property
-    def is_locked(self):
-        return self._is_locked
-
-    async def async_lock(self, **kwargs):
-        await self._api.lock(self._device_id)
-        self._is_locked = True
-        self.async_write_ha_state()
-
-    async def async_unlock(self, **kwargs):
-        await self._api.unlock(self._device_id)
-        self._is_locked = False
-        self.async_write_ha_state()
-
-    async def async_update(self):
-        self._is_locked = await self._api.get_lock_status(self._device_id)
+async def async_setup_entry(
+    hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
+) -> None:
+    """Set up Securitas Alarm locks."""
+   
