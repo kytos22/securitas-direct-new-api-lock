864a90053f3b2c53dacbef60aba2db7886b082cf
Marcos Vidal Martinez <marcos.vidal.m@outlook.com>
2025-01-05 22:50:10 +0100

    Update lock.py

--- custom_components/securitas/lock.py
+++ custom_components/securitas/lock.py
@@ -1,109 +1,59 @@
 """Locks for Securitas Direct."""
 
 import logging
-from typing import Any
-
 from homeassistant.components.lock import LockEntity
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
-
-from .apimanager import ApiManager
-from .const import DOMAIN
-from .coordinator import SecuritasDataUpdateCoordinator
+from .apimanager import APIManager
 
 _LOGGER = logging.getLogger(__name__)
 
 
-async def async_setup_entry(
-    hass: HomeAssistant,
-    entry: dict,
-    async_add_entities: AddEntitiesCallback,
-) -> None:
-    """Set up Securitas Direct locks."""
-    coordinator: SecuritasDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
-    api: ApiManager = coordinator.api
-
-    entities = []
-    devices = coordinator.data.get("devices", [])
-
-    for device in devices:
-        if device.get("type") == "Smart Lock":
-            entities.append(
-                SecuritasLock(
-                    api=api,
-                    coordinator=coordinator,
-                    device_id=device["id"],
-                    name=device["name"],
-                )
-            )
-            _LOGGER.debug(
-                "Added lock entity with ID: %s and name: %s",
-                device["id"],
-                device["name"],
-            )
-
-    async_add_entities(entities)
-
-
 class SecuritasLock(LockEntity):
-    """Representation of a Securitas Direct smart lock."""
+    """Representation of a Securitas Direct Smart Lock."""
 
-    def __init__(
-        self,
-        api: ApiManager,
-        coordinator: SecuritasDataUpdateCoordinator,
-        device_id: str,
-        name: str,
-    ) -> None:
-        """Initialize the lock with API and device info."""
+    def __init__(self, installation, api: APIManager):
+        """Initialize the smart lock."""
+        self._installation = installation
         self._api = api
-        self._coordinator = coordinator
-        self._device_id = device_id
-        self._name = name
-        self._is_locked = None
+        self._is_locked = None  # Will be updated by the state check
+
+    async def async_lock(self, **kwargs):
+        """Lock the smart lock."""
+        try:
+            response = await self._api._change_lock_mode(self._installation, lock=True)
+            if response.get("res") == "OK":
+                self._is_locked = True
+                self.async_write_ha_state()
+            else:
+                _LOGGER.error("Failed to lock the smart lock: %s", response.get("msg"))
+        except Exception as e:
+            _LOGGER.error("Error locking the smart lock: %s", e)
+
+    async def async_unlock(self, **kwargs):
+        """Unlock the smart lock."""
+        try:
+            response = await self._api._change_lock_mode(self._installation, lock=False)
+            if response.get("res") == "OK":
+                self._is_locked = False
+                self.async_write_ha_state()
+            else:
+                _LOGGER.error("Failed to unlock the smart lock: %s", response.get("msg"))
+        except Exception as e:
+            _LOGGER.error("Error unlocking the smart lock: %s", e)
 
     @property
-    def name(self) -> str:
-        """Return the name of the lock."""
-        return self._name
-
-    @property
-    def is_locked(self) -> bool:
+    def is_locked(self):
         """Return true if the lock is locked."""
-        device = self._coordinator.data["devices"].get(self._device_id)
-        if device:
-            status = device.get("lock_status")
-            _LOGGER.debug("Lock %s status: %s", self._device_id, status)
-            return status == "locked"
-        _LOGGER.warning("No status found for lock %s", self._device_id)
-        return False
+        return self._is_locked
 
-    async def async_lock(self, **kwargs: Any) -> None:
-        """Lock the device."""
-        _LOGGER.debug("Locking lock %s", self._device_id)
-        response = await self._api._change_lock_mode(
-            installation=self._coordinator.installation,
-            device_id=self._device_id,
-            lock=True,
-        )
-        if response.get("success"):
-            await self._coordinator.async_request_refresh()
-
-    async def async_unlock(self, **kwargs: Any) -> None:
-        """Unlock the device."""
-        _LOGGER.debug("Unlocking lock %s", self._device_id)
-        response = await self._api._change_lock_mode(
-            installation=self._coordinator.installation,
-            device_id=self._device_id,
-            lock=False,
-        )
-        if response.get("success"):
-            await self._coordinator.async_request_refresh()
-
-    async def async_update(self) -> None:
-        """Fetch the latest status from the API."""
-        status = await self._api._get_lock_status(
-            installation=self._coordinator.installation,
-            device_id=self._device_id,
-        )
-        self._is_locked = status["is_locked"]
+    async def async_update(self):
+        """Fetch the latest state of the lock."""
+        try:
+            status = await self._api.get_lock_current_mode(self._installation)
+            if status == "locked":
+                self._is_locked = True
+            elif status == "unlocked":
+                self._is_locked = False
+            else:
+                _LOGGER.warning("Unknown lock status: %s", status)
+        except Exception as e:
+            _LOGGER.error("Error fetching lock status: %s", e)
